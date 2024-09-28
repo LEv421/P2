7161215170a8d08bd08bc1c1b63c579e51623076
LEv421 <lev.marinchenko@gmail.com>
2024-09-28 14:40:01 +0200

    Create h1.py

---
+++ h1.py
@@ -0,0 +1,26 @@
+class Animal:
+    type = None
+    legs = None
+    tail = None
+    insect = None
+
+    def say():
+        print("Shhhhhhhhhhh")
+
+snake = Animal()
+snake.type = "snake"
+snake.legs = 0
+snake.tail = True
+snake.insect = False
+
+cat = Animal()
+cat.type = "cat"
+cat.legs = 4
+cat.tail = True
+cat.insect = False
+
+ant = Animal()
+ant.type = "ant"
+ant.legs = 6
+ant.tail = True
+ant.insect = True
